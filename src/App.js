6d63fe9aa2bbf3c2fa8ea6c307ba6840a8cb4d5b
vale75020 <valentinosuma@hotmail.com>
2019-07-05 15:17:57 +0200

    map services

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import About from './components/About';
 import Services from './components/Services';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,25 +1,6 @@
 import React, { Component } from "react";
 import "../styles/services.css";
 
-class Services extends Component {
-  render() {
-    return (
-      <div className="container">
-      <div className="services" id="Services">
-        <h3>Services</h3>
-        <h2>What we offer</h2>
-        <div className="row">
-          <Icons myIcon={iconObj[0].icon} myTitle={iconObj[0].title} myDescription={iconObj[0].description} />
-          <Icons myIcon={iconObj[1].icon} myTitle={iconObj[1].title} myDescription={iconObj[1].description} myLink={iconObj[1].link}/>
-          <Icons myIcon={iconObj[2].icon} myTitle={iconObj[2].title} myDescription={iconObj[2].description} />
-          <Icons myIcon={iconObj[3].icon} myTitle={iconObj[3].title} myDescription={iconObj[3].description} myLink={iconObj[3].link} />
-        </div>
-      </div>
-  </div>
-    );
-  }
-}
-
 const iconObj = [
   {
     icon: <span><i className="fas fa-phone"></i></span>,
@@ -45,18 +26,34 @@ const iconObj = [
   }
 ];
 
-class Icons extends Component {
+class Services extends Component {
+  state = {
+    small:true
+  }
   render() {
+    const iconList = iconObj.map(icon => {
+      return (
+              <div className="icon">
+                  <a href={icon.link}>
+                <span>
+                  {icon.icon}
+                </span>
+                </a>
+                <h4>{icon.title}</h4>
+                <p>{icon.description}</p>
+              </div>
+            );})
+
     return (
-      <div className="icon">
-          <a href={this.props.myLink}>
-        <span>
-          {this.props.myIcon}
-        </span>
-        </a>
-        <h4>{this.props.myTitle}</h4>
-        <p>{this.props.myDescription}</p>
+      <div className="container">
+      <div className="services" id="Services">
+        <h3>Services</h3>
+        <h2>What we offer</h2>
+        <div className="row">
+          {iconList}
+        </div>
       </div>
+  </div>
     );
   }
 }
